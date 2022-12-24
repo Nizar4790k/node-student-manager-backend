b5ae82c1dab00cc1a7423c4c9fb9f9034ae7c24c
Nizar4790k <nizar4790k@gmail.com>
2022-12-23 22:29:03 -0400

    el modulo de calificaciones ya funciona

--- controllers/grades.js
+++ controllers/grades.js
@@ -1,47 +1,53 @@
 async function getGrades(req, res, database) {
-    
-  const subject = req.query.subject
-   
+  const { MongoClient, url, dbName } = database;
 
-    
-   
-    const { MongoClient, url, dbName } = database;
-  
-    const client = await MongoClient.connect(url, { useNewUrlParser: true })
-      .catch(err => { console.log(err); });
-  
-    try {
-  
-      if (!client) {
-        res.status(500).json("ERROR IN THE SERVER");
-        return
-      }
-  
-      const db =  client.db(dbName);
-      let collection = db.collection("students");
-  
-      const grades = await collection.find({},{nombre:1,"materias.espanol":1}).toArray()
-      
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+  }).catch((err) => {
+    console.log(err);
+  });
 
-      console.log(grades)
-      
-      
-  
-      res.status(200).json(grades);
-  
-  
-  
-    } catch (err) {
-      console.log(err);
+  try {
+    if (!client) {
       res.status(500).json("ERROR IN THE SERVER");
-    } finally {
-      client.close();
+      return;
     }
 
-    
-  
-  }
+    const db = client.db(dbName);
+    let collection = db.collection("students");
 
-  module.exports = {
-    getGrades:getGrades
+    const subject = req.query.subject;
+
+    var projection = {};
+
+    switch (subject) {
+      case "Lengua Española":
+        projection = { nombre: 1, "materias.espanol": 1 };
+        break;
+      case "Matemáticas":
+        projection = { nombre: 1, "materias.matematicas": 1 };
+        break;
+      case "Ciencias Naturales":
+        projection = { nombre: 1, "materias.naturales": 1 };
+        break;
+      case "Ciencias Sociales":
+        projection = { nombre: 1, "materias.sociales": 1 };
+        break;
+    }
+
+    const grades = await collection
+      .find({}, { projection: projection})
+      .toArray();
+
+    res.status(200).json(grades);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("ERROR IN THE SERVER");
+  } finally {
+    client.close();
   }
+}
+
+module.exports = {
+  getGrades: getGrades,
+};
